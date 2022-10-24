45f28e49284e57dd5f2a3b8452ae1bdea2b579c2
Natalie Esseen <natalieesseen@gmail.com>
2022-10-24 11:06:15 +0200

    first iteration of my personal page ✨

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className='home-container'>
+          <h1>Hello, my name is <br></br>Natalie Esseen</h1>
+          <div className='text-block'>
+            <p>👋 I'm a web developer based in Umeå, Sweden. I love creating pretty and user-friendly interfaces, web apps, and everything in between!</p>
+          </div>
+        </div>
+
+        <img src='/img-1.png' alt='natalie' className='about-img-1' />
+        <img src='/img-2.png' alt='cat with book' className='about-img-2' />
+
+        <img src='/scallop-edge-mint.svg' alt='scallop svg' className='deco'/>
+        <div className='about-container'>
+          <div className='text-block'>
+          <p>Currently studying my last year at Changemaker Educations's program <i>Web Developer, E-commerce.</i></p>
+        
+          <p>Here are a few of the technologies and tools I work with:</p>
+          </div>
+          <div className='text-block-small'>
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li> PHP</li>
+              <li>Adobe Creative Suite</li>
+              <li>Figma</li>
+            </ul>
+          </div>
+          <div className='text-block'>
+           <a href='Natalie-CV2022.pdf' target={'_blank'} className='resume-link'>Take a look at my resume</a>
+          </div>
+        </div>
+
+        <div className='project-container'>
+          <h1>Projects</h1>
+          <p>Oops! I'm working on it! If you want to take a look at a few of my previous projects, you can do so over at my <a href='https://github.com/natalieesseen'>github</a> ☕</p>
+        </div>
+
+        <img src='/scallop-edge-purple.svg' alt='scallop svg' className='deco'/>
+        <div className='contact-container'>
+          <h1>Natalie Esseen</h1>
+          <p>natalieesseen@gmail.com</p>
+        </div>
+        <div className='footer'>
+            <p>Designed & Built by Natalie Esseen</p>
+        </div>
     </div>
   );
 }
