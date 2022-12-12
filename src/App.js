0008da569410978b36ac8fe0e536d8f641230187
Natalie Esseen <natalieesseen@gmail.com>
2022-12-12 13:29:53 +0100

    new img + styling 🌷

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
         <div className='home-container'>
           <h1>Hello, my name is<br />Natalie Esseen</h1>
           <div className='text-block'>
-            <p>👋 I'm a web developer based in Umeå, Sweden. I love creating pretty and user-friendly interfaces, web apps, and everything in between!</p>
+            <p>👋 I'm a web developer based in Umeå, Sweden. I enjoy creating aestethic and user-friendly interfaces, web apps, and almost everything in between!</p>
           </div>
+          <img src='/img-0112.png' alt='natalie' className='home-img' />
         </div>
-
-        <img src='/img-wide.png' alt='natalie & cat' className='about-img' />
 
         <img src='/scallop-edge-mint.svg' alt='scallop svg' className='deco'/>
         <div className='about-container'>
           <div className='text-block'>
-          <p>Currently studying my last year at Changemaker Educations's program <i>Web Developer, E-commerce.</i></p>
+          <p>Currently studying my last year at Changemaker Educations's program <i>Web Developer, E-commerce.</i> And will start my internship in January 2023 as a junior developer!</p>
         
           <p>Here are a few of the technologies and tools I work with:</p>
           </div>
@@ -30,20 +29,18 @@ function App() {
               <li>Figma</li>
             </ul>
           </div>
-          <div className='text-block'>
-           <p><a href='https://shadow-stitch-17b.notion.site/Natalie-Esseen-fdd2c6b9ea424b1bbb2e3dadd9f68f0f' className='resume-link'>Link to my resume ✨</a></p>
-          </div>
         </div>
 
         <div className='project-container'>
           <h1>Projects</h1>
-          <p>Oops! I'm working on it! If you want to take a look at a few of my previous projects, you can do so over at my <a href='https://github.com/natalieesseen'>github</a> ☕</p>
+          <p>Oops! I'm working on it! If you want to take a look at a few of my previous projects, you can do so over at my <a href='https://github.com/natalieesseen'>GitHub</a> ☕</p>
         </div>
 
         <img src='/scallop-edge-purple.svg' alt='scallop svg' className='deco'/>
         <div className='contact-container'>
           <h1>Natalie Esseen</h1>
-          <p>natalieesseen@gmail.com</p>
+            <p>Feel free to <a href='mailto:natalieesseen@gmail.com' className='footer-a'>Say Hello</a> or take a closer look at my
+            <a href='https://shadow-stitch-17b.notion.site/Natalie-Esseen-fdd2c6b9ea424b1bbb2e3dadd9f68f0f' className='footer-a'> Resume</a> 🌷</p>
         </div>
         <div className='footer'>
           <p><a href='https://github.com/natalieesseen/personal-page' className='footer-a'>Designed & Built by Natalie Esseen</a></p>
